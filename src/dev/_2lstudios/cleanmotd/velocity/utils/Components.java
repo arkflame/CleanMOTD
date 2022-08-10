f40973715afe08f26be97468b59d05a412a64cb2
Adrian3d04 <theadrian04@gmail.com>
2022-08-10 17:35:56 +0000

    Fixed legacy character parse

--- src/dev/_2lstudios/cleanmotd/velocity/utils/Components.java
+++ src/dev/_2lstudios/cleanmotd/velocity/utils/Components.java
@@ -3,6 +3,9 @@ package dev._2lstudios.cleanmotd.velocity.utils;
 import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
 
 public final class Components {
+    private Components(){}
     public static final LegacyComponentSerializer SERIALIZER = LegacyComponentSerializer.builder()
-        .hexColors().build();
+        .character(LegacyComponentSerializer.AMPERSAND_CHAR)
+        .hexColors()
+        .build();
 }
